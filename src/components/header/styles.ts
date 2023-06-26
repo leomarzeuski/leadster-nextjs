ee42620ede193c2435e51b585d0d4d544d201e45
Leonardo Marzeuski <leomarzeuskii@gmail.com>
2023-06-26 20:16:02 -0300

    header

--- src/components/header/styles.ts
+++ src/components/header/styles.ts
@@ -3,11 +3,15 @@ import { AiOutlineMenu } from "react-icons/ai";
 import Image from "next/image";
 
 export const HeaderContainer = styled.header`
+  position: sticky;
+  top: 0;
   height: 6.5em;
   background-color: white;
   display: flex;
   justify-content: center;
   align-items: center;
+  z-index: 100;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.1);
 
   @media (max-width: 768px) {
     justify-content: space-between;
@@ -26,7 +30,7 @@ export const MenuIcon = styled(AiOutlineMenu)<{ open: boolean }>`
   @media (max-width: 768px) {
     display: block;
     z-index: 1;
-    width: 100%;
+    width: auto;
     height: 40px;
     color: blue;
     cursor: pointer;
