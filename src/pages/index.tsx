ba9c7ee0a1dccacc6a6f62c8a8a8f0bc8fad1fee
Leonardo Marzeuski <leomarzeuskii@gmail.com>
2023-06-25 22:47:47 -0300

    page

---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import Header from "@/components/header";
+import Hero from "@/components/hero";
+import OurVideos from "@/components/ourVideos";
+
+const LandingPage: React.FC = () => {
+  return (
+    <>
+      <Header />
+      <Hero />
+      <OurVideos />
+    </>
+  );
+};
+
+export default LandingPage;
