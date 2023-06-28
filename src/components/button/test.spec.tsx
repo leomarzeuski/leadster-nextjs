39d0e1746b9bf290c1b8dd779cf01c5474bcc23a
Leonardo Marzeuski <leomarzeuskii@gmail.com>
2023-06-28 00:39:48 -0300

    teste button

--- src/components/button/test.spec.tsx
+++ src/components/button/test.spec.tsx
@@ -1,13 +1,10 @@
-import React from "react";
-import Button from ".";
 import { render, screen } from "@testing-library/react";
-import userEvent from "@testing-library/user-event";
+import Button from ".";
 
 describe("Button", () => {
   it("should render the button with the correct label", () => {
     const label = "Test button";
     render(<Button label={label} />);
-
     const button = screen.getByText(label);
     expect(button).toBeTruthy();
   });
